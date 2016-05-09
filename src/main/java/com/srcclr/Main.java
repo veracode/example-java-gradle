e03ee225fa03e0d9840efea065014c8f0dc8a621
Jason Yeo <jasonyeo88@gmail.com>
2016-05-09 12:48:38 +0800

    Add sauces

---
+++ src/main/java/com/srcclr/Main.java
@@ -0,0 +1,13 @@
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String candidate = args[0];
+    String hashed = BCrypt.hashpw(candidate, BCrypt.gensalt(12));
+
+    BCrypt.checkpw(candidate, hashed);
+  }
+
+}
+
