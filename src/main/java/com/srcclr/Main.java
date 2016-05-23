6a120745962957d25e5f41f66a05769100f17d3c
Darius Foo <darius.foo.tw@gmail.com>
2016-05-23 14:03:59 +0800

    Added calls to vulnerable methods

--- src/main/java/com/srcclr/Main.java
+++ src/main/java/com/srcclr/Main.java
@@ -1,4 +1,11 @@
+package com.srcclr;
+
+import org.apache.commons.fileupload.MultipartStream;
+import org.apache.xml.security.signature.XMLSignatureInput;
 import org.mindrot.jbcrypt.BCrypt;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 public class Main {
 
@@ -7,7 +14,18 @@ public class Main {
     String hashed = BCrypt.hashpw(candidate, BCrypt.gensalt(12));
 
     BCrypt.checkpw(candidate, hashed);
+
+    filterXMLSignature();
   }
 
-}
+  private static void filterXMLSignature() {
+    byte[] bytes = new byte[256];
 
+    try {
+      new MultipartStream(new ByteArrayInputStream(bytes), bytes);
+    } catch (IOException ignored) {
+    }
+
+    new XMLSignatureInput(bytes).addNodeFilter(null);
+  }
+}
